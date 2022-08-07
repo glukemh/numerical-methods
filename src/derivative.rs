df9d006c5f051bf30d86bdd1baad98c2a338ef04
Luke Mendelman-Haenn <glukemh@gmail.com>
2022-08-07 17:12:26 -0400

    fixed derivative rounding error

--- src/derivative.rs
+++ src/derivative.rs
@@ -1,32 +1,43 @@
-// pub fn d_dx(f: &dyn Fn(f64) -> f64, x: f64) -> f64 {
-//   (f(x + f64::EPSILON) - f(x)) / f64::EPSILON
-// }
+use crate::floats::FloatOps;
+const SQRT_EPSILON: f64 = 0.000000014901161193847656;
 
-// #[cfg(test)]
-// mod tests {
-//   use super::*;
+pub fn d_dx(f: &dyn Fn(f64) -> f64, x: f64) -> f64 {
+    let h = if x == 0.0 {
+        f64::EPSILON
+    } else {
+        SQRT_EPSILON * x
+    };
+    (f(x + h) - f(x)) / h
+}
 
-//   fn assert_close(x: f64, y: f64, eps: f64) {
-//     println!("left: {}", x);
-//     println!("right: {}", y);
-//     assert!((x - y).abs() < eps);
-//   }
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-//   #[test]
-//   fn test_d_dx_1() {
-//     let f = |x: f64| x.powi(2);
-//     assert_close(d_dx(&f, 0.0), 0.0, 1e-10);
-//   }
+    fn assert_close(a: f64, b: f64, eps: f64) {
+        println!("left: {}", a);
+        println!("right: {}", b);
+        assert!((a - b).abs() < eps);
+    }
 
-//   #[test]
-//   fn test_d_dx_2() {
-//     let f = |x: f64| x.powi(2);
-//     assert_close(d_dx(&f, 3.0), 6.0, 1e-10);
-//   }
+    #[test]
+    fn test_d_dx_1() {
+        let f = |x: f64| x.powi(2);
+        let x = 0.0;
+        assert_close(d_dx(&f, x), 0.0, 1e-7);
+    }
 
-//   #[test]
-//   fn test_d_dx_3() {
-//     let f = |x: f64| 1.0 / x;
-//     assert_close(d_dx(&f, 5.0), -0.04, 1e-10);
-//   }
-// }
+    #[test]
+    fn test_d_dx_2() {
+        let f = |x: f64| x.powi(2);
+        let x = 3.0;
+        assert_close(d_dx(&f, x), 6.0, 1e-7);
+    }
+
+    #[test]
+    fn test_d_dx_3() {
+        let f = |x: f64| 1.0 / x;
+        let x = 5.0;
+        assert_close(d_dx(&f, x), -0.04, 1e-6);
+    }
+}
--- src/floats.rs
+++ src/floats.rs
@@ -1,43 +1,55 @@
+const MANTISSA_BITS_64: u64 = 0x000f_ffff_ffff_ffff;
+const EXPONENT_BITS_64: u64 = 0x7ff0_0000_0000_0000;
+const SIGN_BIT_64: u64 = 0x8000_0000_0000_0000;
+const UNIT_64: u64 = 0x0000_0000_0000_0001;
+const EXPONENT_UNIT_64: u64 = MANTISSA_BITS_64 + UNIT_64;
+
+const MANTISSA_BITS_32: u32 = 0x007f_ffff;
+const EXPONENT_BITS_32: u32 = 0x7f80_0000;
+const SIGN_BIT_32: u32 = 0x8000_0000;
+const UNIT_32: u32 = 0x0000_0001;
+const EXPONENT_UNIT_32: u32 = MANTISSA_BITS_32 + UNIT_32;
+
 pub trait FloatOps<T> {
-  fn mantissa(self) -> T;
-  fn exponent(self) -> T;
-  fn next(self) -> Self;
+    fn mantissa(self) -> T;
+    fn exponent(self) -> T;
+    fn next(self) -> Self;
 }
 
 impl FloatOps<u64> for f64 {
-  fn mantissa(self) -> u64 {
-    self.to_bits() & 0x000f_ffff_ffff_ffff as u64
-  }
-  fn exponent(self) -> u64 {
-    self.to_bits() & 0x7ff0_0000_0000_0000 as u64
-  }
-  fn next(self) -> f64 {
-    println!("mantissa: {}", self.mantissa());
-    println!("mantissa + 1: {}", self.mantissa() + 1);
-    if self.mantissa() == 0x000f_ffff_ffff_ffff as u64 {
-      f64::from_bits(
-        self as u64 & 0x8000_0000_0000_0000 | (self.exponent() + 0x0010_0000_0000_0000),
-      )
-    } else {
-      f64::from_bits(self as u64 & 0x8000_0000_0000_0000 | self.exponent() | (self.mantissa() + 1))
+    fn mantissa(self) -> u64 {
+        self.to_bits() & MANTISSA_BITS_64 as u64
     }
-  }
+    fn exponent(self) -> u64 {
+        self.to_bits() & EXPONENT_BITS_64 as u64
+    }
+    fn next(self) -> f64 {
+        if self.mantissa() == MANTISSA_BITS_64 as u64 {
+            f64::from_bits(self as u64 & SIGN_BIT_64 | (self.exponent() + EXPONENT_UNIT_64))
+        } else {
+            f64::from_bits(
+                self as u64 & SIGN_BIT_64 | self.exponent() | (self.mantissa() + UNIT_64),
+            )
+        }
+    }
 }
 
 impl FloatOps<u32> for f32 {
-  fn mantissa(self) -> u32 {
-    self.to_bits() & 0x007f_ffff as u32
-  }
-  fn exponent(self) -> u32 {
-    self.to_bits() & 0x7f80_0000 as u32
-  }
-  fn next(self) -> f32 {
-    if self.mantissa() == 0x007f_ffff as u32 {
-      f32::from_bits(self as u32 & 0x8000_0000 | (self.exponent() + 0x0080_0000))
-    } else {
-      f32::from_bits(self as u32 & 0x8000_0000 | self.exponent() | (self.mantissa() + 1))
+    fn mantissa(self) -> u32 {
+        self.to_bits() & MANTISSA_BITS_32 as u32
     }
-  }
+    fn exponent(self) -> u32 {
+        self.to_bits() & EXPONENT_BITS_32 as u32
+    }
+    fn next(self) -> f32 {
+        if self.mantissa() == MANTISSA_BITS_32 as u32 {
+            f32::from_bits(self as u32 & SIGN_BIT_32 | (self.exponent() + EXPONENT_UNIT_32))
+        } else {
+            f32::from_bits(
+                self as u32 & SIGN_BIT_32 | self.exponent() | (self.mantissa() + UNIT_32),
+            )
+        }
+    }
 }
 
 // pub fn next_64(x: f64) -> f64 {
@@ -55,109 +67,109 @@ impl FloatOps<u32> for f32 {
 
 #[cfg(test)]
 mod tests {
-  use super::*;
+    use super::*;
 
-  // f64 tests
-  #[test]
-  fn test_mantissa_64_1() {
-    assert_eq!(1f64.mantissa(), 0);
-  }
+    // f64 tests
+    #[test]
+    fn test_mantissa_64_1() {
+        assert_eq!(1f64.mantissa(), 0);
+    }
 
-  #[test]
-  fn test_mantissa_64_2() {
-    assert_eq!(256f64.mantissa(), 0);
-  }
+    #[test]
+    fn test_mantissa_64_2() {
+        assert_eq!(256f64.mantissa(), 0);
+    }
 
-  #[test]
-  fn test_mantissa_64_3() {
-    assert_eq!(0.125f64.mantissa(), 0);
-  }
+    #[test]
+    fn test_mantissa_64_3() {
+        assert_eq!(0.125f64.mantissa(), 0);
+    }
 
-  #[test]
-  fn test_mantissa_64_4() {
-    assert_eq!((1.0 + f64::EPSILON).mantissa(), 1);
-  }
+    #[test]
+    fn test_mantissa_64_4() {
+        assert_eq!((1.0 + f64::EPSILON).mantissa(), 1);
+    }
 
-  #[test]
-  fn test_mantissa_64_5() {
-    assert_eq!((1.0 + 123.0 * f64::EPSILON).mantissa(), 123);
-  }
+    #[test]
+    fn test_mantissa_64_5() {
+        assert_eq!((1.0 + 123.0 * f64::EPSILON).mantissa(), 123);
+    }
 
-  #[test]
-  fn test_mantissa_64_6() {
-    let n = 0x000eeeeeeeeeeeee as u64;
-    assert_eq!((1.0 + n as f64 * f64::EPSILON).mantissa(), n);
-  }
+    #[test]
+    fn test_mantissa_64_6() {
+        let n = 0x000eeeeeeeeeeeee as u64;
+        assert_eq!((1.0 + n as f64 * f64::EPSILON).mantissa(), n);
+    }
 
-  #[test]
-  fn test_exponent_64_1() {
-    assert_eq!(f64::INFINITY.exponent(), 0x7ff0_0000_0000_0000);
-  }
+    #[test]
+    fn test_exponent_64_1() {
+        assert_eq!(f64::INFINITY.exponent(), 0x7ff0_0000_0000_0000);
+    }
 
-  #[test]
-  fn test_exponent_64_2() {
-    assert_eq!(1f64.exponent(), 0x3ff0_0000_0000_0000);
-  }
+    #[test]
+    fn test_exponent_64_2() {
+        assert_eq!(1f64.exponent(), 0x3ff0_0000_0000_0000);
+    }
 
-  #[test]
-  fn test_exponent_64_3() {
-    assert_eq!((-1f64).exponent(), 0x3ff0_0000_0000_0000);
-  }
+    #[test]
+    fn test_exponent_64_3() {
+        assert_eq!((-1f64).exponent(), 0x3ff0_0000_0000_0000);
+    }
 
-  #[test]
-  fn test_next_64_1() {
-    assert_eq!(1f64.next(), 1.0 + f64::EPSILON);
-  }
+    #[test]
+    fn test_next_64_1() {
+        assert_eq!(1f64.next(), 1.0 + f64::EPSILON);
+    }
 
-  // f32 tests
-  #[test]
-  fn test_mantissa_32_1() {
-    assert_eq!(1f32.mantissa(), 0);
-  }
+    // f32 tests
+    #[test]
+    fn test_mantissa_32_1() {
+        assert_eq!(1f32.mantissa(), 0);
+    }
 
-  #[test]
-  fn test_mantissa_32_2() {
-    assert_eq!(256f32.mantissa(), 0);
-  }
+    #[test]
+    fn test_mantissa_32_2() {
+        assert_eq!(256f32.mantissa(), 0);
+    }
 
-  #[test]
-  fn test_mantissa_32_3() {
-    assert_eq!(0.125f32.mantissa(), 0);
-  }
+    #[test]
+    fn test_mantissa_32_3() {
+        assert_eq!(0.125f32.mantissa(), 0);
+    }
 
-  #[test]
-  fn test_mantissa_32_4() {
-    assert_eq!((1.0 + f32::EPSILON).mantissa(), 1);
-  }
+    #[test]
+    fn test_mantissa_32_4() {
+        assert_eq!((1.0 + f32::EPSILON).mantissa(), 1);
+    }
 
-  #[test]
-  fn test_mantissa_32_5() {
-    assert_eq!((1.0 + 123.0 * f32::EPSILON).mantissa(), 123);
-  }
+    #[test]
+    fn test_mantissa_32_5() {
+        assert_eq!((1.0 + 123.0 * f32::EPSILON).mantissa(), 123);
+    }
 
-  #[test]
-  fn test_mantissa_32_6() {
-    let n = 0x00777777 as u32;
-    assert_eq!((1.0 + n as f32 * f32::EPSILON).mantissa(), n);
-  }
+    #[test]
+    fn test_mantissa_32_6() {
+        let n = 0x00777777 as u32;
+        assert_eq!((1.0 + n as f32 * f32::EPSILON).mantissa(), n);
+    }
 
-  #[test]
-  fn test_exponent_32_1() {
-    assert_eq!(f32::INFINITY.exponent(), 0x7f80_0000);
-  }
+    #[test]
+    fn test_exponent_32_1() {
+        assert_eq!(f32::INFINITY.exponent(), 0x7f80_0000);
+    }
 
-  #[test]
-  fn test_exponent_32_2() {
-    assert_eq!(1f32.exponent(), 0x3f80_0000);
-  }
+    #[test]
+    fn test_exponent_32_2() {
+        assert_eq!(1f32.exponent(), 0x3f80_0000);
+    }
 
-  #[test]
-  fn test_exponent_32_3() {
-    assert_eq!((-1f32).exponent(), 0x3f80_0000);
-  }
+    #[test]
+    fn test_exponent_32_3() {
+        assert_eq!((-1f32).exponent(), 0x3f80_0000);
+    }
 
-  #[test]
-  fn test_next_32_1() {
-    assert_eq!(1f32.next(), 1.0 + f32::EPSILON);
-  }
+    #[test]
+    fn test_next_32_1() {
+        assert_eq!(1f32.next(), 1.0 + f32::EPSILON);
+    }
 }
